22deb23246545a0463587a0b57bacc01e56a901e
CHristapherenese <64864560+CHristapherenese@users.noreply.github.com>
2020-06-08 21:53:17 -0400

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,16 +1,29 @@
 function barkTone () {
+    music.pewPew.play()
+    light.showRing(
+    `red red red red red red black red red red`
+    )
     pins.A1.digitalWrite(true)
     light.setPixelColor(6, 0xffff00)
     pause(5000)
     pins.A1.digitalWrite(false)
-    light.setPixelColor(6, 0x000000)
+    light.setAll(0x000000)
 }
 input.onLoudSound(function () {
     barkTone()
 })
+function boot () {
+    for (let index = 0; index < 3; index++) {
+        music.playTone(988, music.beat(BeatFraction.Whole))
+        pause(1000)
+    }
+    light.showAnimation(light.rainbowAnimation, 1000)
+    light.stopAllAnimations()
+    light.setAll(0x000000)
+}
 input.buttonA.onEvent(ButtonEvent.Click, function () {
     barkTone()
 })
-forever(function () {
-    input.setLoudSoundThreshold(150)
-})
+input.setLoudSoundThreshold(200)
+music.setVolume(100)
+boot()
